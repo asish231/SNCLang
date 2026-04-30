9ca745e085c7ed165bd16675a2bb2764de9955bb
asish231 <asishkksharma@gmail.com>
2026-04-30 11:05:07 +0530

    refactor: optimize stack usage and variable handling in _main function

--- out.s
+++ out.s
@@ -17,46 +17,37 @@
 _main:
     stp x29, x30, [sp, #-16]!
     mov x29, sp
-    sub sp, sp, #32
+    sub sp, sp, #16
     adrp x9, store_val_0@PAGE
     ldr x10, [x9, store_val_0@PAGEOFF]
     stur x10, [x29, #-8]
-    adrp x9, store_val_1@PAGE
-    ldr x10, [x9, store_val_1@PAGEOFF]
-    stur x10, [x29, #-16]
+L_snl_1:
+    ldur x11, [x29, #-8]
     adrp x9, store_val_2@PAGE
     ldr x10, [x9, store_val_2@PAGEOFF]
-    stur x10, [x29, #-24]
-    ldur x11, [x29, #-8]
-    ldur x10, [x29, #-16]
-    add x11, x11, x10
-    stur x11, [x29, #-24]
+    cmp x11, x10
+    cset x11, lt
+    stur x11, [x29, #-16]
+    ldur x11, [x29, #-16]
+    cbz x11, L_snl_2
     adrp x0, print_fmt_int@PAGE
     add x0, x0, print_fmt_int@PAGEOFF
-    ldur x1, [x29, #-24]
+    ldur x1, [x29, #-8]
     sub sp, sp, #16
     str x1, [sp]
     bl _printf
     add sp, sp, #16
     ldur x11, [x29, #-8]
-    ldur x10, [x29, #-16]
-    mul x11, x11, x10
-    stur x11, [x29, #-24]
-    adrp x0, print_fmt_int@PAGE
-    add x0, x0, print_fmt_int@PAGEOFF
-    ldur x1, [x29, #-24]
-    sub sp, sp, #16
-    str x1, [sp]
-    bl _printf
-    add sp, sp, #16
-    ldur x11, [x29, #-8]
-    adrp x9, store_val_7@PAGE
-    ldr x10, [x9, store_val_7@PAGEOFF]
+    adrp x9, store_val_5@PAGE
+    ldr x10, [x9, store_val_5@PAGEOFF]
     add x11, x11, x10
-    stur x11, [x29, #-24]
+    stur x11, [x29, #-8]
+    b L_snl_1
+L_snl_2:
     adrp x0, print_fmt_int@PAGE
     add x0, x0, print_fmt_int@PAGEOFF
-    ldur x1, [x29, #-24]
+    adrp x9, print_val_0@PAGE
+    ldr x1, [x9, print_val_0@PAGEOFF]
     sub sp, sp, #16
     str x1, [sp]
     bl _printf
@@ -85,17 +76,17 @@ dec_sign_minus:
     .asciz "-"
 .align 3
 .align 3
+print_val_0:
+    .quad 99
+.align 3
 store_val_0:
-    .quad 5
-.align 3
-store_val_1:
-    .quad 3
-.align 3
-store_val_2:
     .quad 0
 .align 3
-store_val_7:
-    .quad 7
+store_val_2:
+    .quad 5
+.align 3
+store_val_5:
+    .quad 1
 .align 3
 list_pool_values:
 list_pool_lengths:
